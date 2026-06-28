be275d0ba2cbcac2d9780e6d455a1c230ef56c67
Leonardo Bitzki <ljbitzki@gmail.com>
2026-06-28 16:54:29 +0000

    New setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+
 echo "Installing required packages..."
 sudo apt update
 sudo DEBIAN_FRONTEND=noninteractive apt install -y tshark tcpdump python3-venv cmake wireshark redis ca-certificates curl
 sudo DEBIAN_FRONTEND=noninteractive dpkg-reconfigure wireshark-common
 sudo chmod +x /usr/bin/dumpcap
 sudo setcap cap_net_raw,cap_net_admin=eip "$(command -v tcpdump)"
+
 echo "Installing tools..."
 chmod +x clients.sh docker-install.sh servers.sh build.sh environ.sh
 python3 -m venv .venv
@@ -13,6 +15,7 @@ pip install --upgrade pip
 pip install -r requirements.txt
 git clone https://github.com/ahlashkari/NTLFlowLyzer.git
 cd NTLFlowLyzer
+
 echo -e "\nsetuptools" >> requirements.txt
 pip install -r requirements.txt
 python3 setup.py install
