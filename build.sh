26dd4455bb0e502354aaf69df63a81e4472e91f0
Leonardo Bitzki <ljbitzki@gmail.com>
2026-06-28 16:57:28 +0000

    Build script

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
+
 echo "Building containers..."
 cd docker/ || exit 1
 chmod +x build-images.sh
 ./build-images.sh
+
 echo "Containers created..."
 cd ../
 source .venv/bin/activate
+exit 0
